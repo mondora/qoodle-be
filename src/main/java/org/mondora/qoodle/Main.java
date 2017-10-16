a4feece354883b5c381c7e097c888440e0c36d8d
jab <jab@localhost.localdomain>
2017-10-16 17:50:56 +0200

    riformattato tutto

--- src/main/java/org/mondora/qoodle/Main.java
+++ src/main/java/org/mondora/qoodle/Main.java
@@ -18,20 +18,14 @@ import spark.Response;
 public class Main {
 
 
-
-
-
-
-    private static String showUserToken(Gson gson, Request req)
-    {
+    private static String showUserToken(Gson gson, Request req) {
 
         org.mondora.qoodle.AuthObject recivedObject = gson.fromJson(req.body(), org.mondora.qoodle.AuthObject.class);
 
         return (recivedObject.verify(gson));
     }
 
-    public static boolean isLoggedIn(Request req)
-    {
+    public static boolean isLoggedIn(Request req) {
 
         // System.out.println("QOESTA È LA REQUEWST la mail" + req.headers("email") + req.headers("id_token")  + "   " + req.headers("id_client"));
 
@@ -42,12 +36,9 @@ public class Main {
     }
 
 
+    public static String getDetails(Datastore ds, Gson gson, Request req) {
 
-
-    public static String getDetails(Datastore ds , Gson gson, Request req)
-    {
-
-        if(isLoggedIn(req)) {
+        if (isLoggedIn(req)) {
 
             long id = Long.parseLong(req.params(":id"));
 
@@ -55,7 +46,7 @@ public class Main {
             final org.mondora.qoodle.Qoodle targetQoodle = primaQuery.limit(1).get();
 
             int nrElements = 0;
-            if(targetQoodle.getQeList() != null) nrElements = targetQoodle.getQeList().size();
+            if (targetQoodle.getQeList() != null) nrElements = targetQoodle.getQeList().size();
             final int nrUser = targetQoodle.getVoList().size();
             Detail[] details = new Detail[nrElements];
 
@@ -82,23 +73,18 @@ public class Main {
             Details d = new Details(targetQoodle.getTitle(), details, targetQoodle.getType());
 
             return gson.toJson(d);
-        }else
-        {
+        } else {
             return "ACCESSO VIETATO";
         }
 
     }
 
 
-
-
-
-
     private static String getList(Datastore datastore, Gson gson, Request req) {
 
 
-        if(isLoggedIn(req)) {
-            final List<org.mondora.qoodle.Qoodle> qoodles  = datastore.createQuery(org.mondora.qoodle.Qoodle.class).retrievedFields(true, "qoodleId", "title", "description", "closingDate", "voList", "backgroundImage", "owner").asList();
+        if (isLoggedIn(req)) {
+            final List<org.mondora.qoodle.Qoodle> qoodles = datastore.createQuery(org.mondora.qoodle.Qoodle.class).retrievedFields(true, "qoodleId", "title", "description", "closingDate", "voList", "backgroundImage", "owner").asList();
 
 
             ArrayList<org.mondora.qoodle.Qoodles> qList = new ArrayList<>();
@@ -119,19 +105,17 @@ public class Main {
             }
 
             return gson.toJson(qList);
-        }
-        else
-        {
+        } else {
             return "ACCESSO VIETATO";
         }
     }
 
-    private static String getQoodleView(Gson gson,Datastore datastore, Request req) {
+    private static String getQoodleView(Gson gson, Datastore datastore, Request req) {
 
-        if(isLoggedIn(req)) {
-            long id = Long.parseLong( req.params(":id"));
+        if (isLoggedIn(req)) {
+            long id = Long.parseLong(req.params(":id"));
 
-            final Query<org.mondora.qoodle.Qoodle> primaQuery = datastore.createQuery(org.mondora.qoodle.Qoodle.class).filter("qoodleId ==", id).retrievedFields(true, "qoodleId","title", "description","closingDate", "qeList", "type");
+            final Query<org.mondora.qoodle.Qoodle> primaQuery = datastore.createQuery(org.mondora.qoodle.Qoodle.class).filter("qoodleId ==", id).retrievedFields(true, "qoodleId", "title", "description", "closingDate", "qeList", "type");
             final org.mondora.qoodle.Qoodle targetQoodle = primaQuery.limit(1).get();
 
 
@@ -140,82 +124,81 @@ public class Main {
 
                             targetQoodle.getQoodleId(),
                             targetQoodle.getTitle(),
-                            targetQoodle.getDescription() ,
+                            targetQoodle.getDescription(),
                             targetQoodle.getClosingDate(),
                             targetQoodle.getQeList(),
                             targetQoodle.getType()
                     );
 
-            if(targetQoodle.getQeList() == null )
+            if (targetQoodle.getQeList() == null)
                 qView.setEle(new ArrayList<>());
 
             return gson.toJson(qView);
-        }
-        else
-        {
+        } else {
             return "ACCESSO VIETATO";
         }
     }
 
-    private static Response submitVotes(Datastore datastore, Gson gson, Request req,  Response res) {
+    private static Response submitVotes(Datastore datastore, Gson gson, Request req, Response res) {
 
-        if (isLoggedIn(req)) {
-            final org.mondora.qoodle.VoteRequest completeObject = gson.fromJson(req.body().toString(), org.mondora.qoodle.VoteRequest.class);
-            final org.mondora.qoodle.Vote newVote = new org.mondora.qoodle.Vote(completeObject.getUserId(), completeObject.getRealName(), completeObject.getVotes());
+        try{
 
-            final Query<org.mondora.qoodle.Qoodle> updateQuery = datastore.createQuery(org.mondora.qoodle.Qoodle.class).filter("qoodleId ==", completeObject.getQoodleId());
-            final UpdateOperations<org.mondora.qoodle.Qoodle> updateQoodleVote;
+            if (isLoggedIn(req)) {
+                final org.mondora.qoodle.VoteRequest completeObject = gson.fromJson(req.body().toString(), org.mondora.qoodle.VoteRequest.class);
+                final org.mondora.qoodle.Vote newVote = new org.mondora.qoodle.Vote(completeObject.getUserId(), completeObject.getRealName(), completeObject.getVotes());
+
+                final Query<org.mondora.qoodle.Qoodle> updateQuery = datastore.createQuery(org.mondora.qoodle.Qoodle.class).filter("qoodleId ==", completeObject.getQoodleId());
+                final UpdateOperations<org.mondora.qoodle.Qoodle> updateQoodleVote;
 
 
+                if (!updateQuery.get().getVoList().contains(newVote)) {
+                    ArrayList<Vote> withNewVote = updateQuery.get().getVoList();
+                    withNewVote.add(newVote);
+                    updateQoodleVote = datastore.createUpdateOperations(org.mondora.qoodle.Qoodle.class).set("voList", withNewVote);
+                } else {//se esiste lo sostituisco
+                    updateQuery.get().getVoList().set(updateQuery.get().getVoList().indexOf(newVote), newVote);
 
-            if (!updateQuery.get().getVoList().contains(newVote)) {
-                ArrayList<Vote> withNewVote = updateQuery.get().getVoList();
-                withNewVote.add(newVote);
-                updateQoodleVote = datastore.createUpdateOperations(org.mondora.qoodle.Qoodle.class).set("voList", withNewVote);
-            } else {//se esiste lo sostituisco
-                updateQuery.get().getVoList().set(updateQuery.get().getVoList().indexOf(newVote), newVote);
+                    updateQoodleVote = datastore.createUpdateOperations(org.mondora.qoodle.Qoodle.class).set("voList", updateQuery.get().getVoList());
 
-                updateQoodleVote = datastore.createUpdateOperations(org.mondora.qoodle.Qoodle.class).set("voList", updateQuery.get().getVoList());
+                }
 
+                datastore.update(updateQuery, updateQoodleVote);
+
+                res.status(200);
+            } else {
+                res.status(401);
             }
-
-            datastore.update(updateQuery, updateQoodleVote);
-
-            res.status(200);
-        } else {
-            res.status(401);
+        } catch (Exception e) {
+            e.printStackTrace();
+            res.status(500);
         }
-
         return res;
     }
 
     private static Response saveQoodle(String targetId, Request req, Response res, Gson gson, Datastore datastore) {
 
-        try{
-            if(isLoggedIn(req)) {
+        try {
+            if (isLoggedIn(req)) {
                 final org.mondora.qoodle.Qoodle primoQoodle = gson.fromJson(req.body().toString(), org.mondora.qoodle.Qoodle.class);
 
                 primoQoodle.insert(targetId, datastore);
                 res.status(200);
-            }
-            else
-            {
+            } else {
 
                 res.status(401);
             }
-        }catch (Exception e)
-        {
+        } catch (Exception e) {
             e.printStackTrace();
             res.status(500);
         }
 
-            return res;
+        return res;
 
 
     }
 
-    private static String getQoodleElements( Gson gson, Datastore datastore, Request req) {
-        if(isLoggedIn(req)) {
+    private static String getQoodleElements(Gson gson, Datastore datastore, Request req) {
+        if (isLoggedIn(req)) {
             final org.mondora.qoodle.Qoodle templateExample = datastore.createQuery(org.mondora.qoodle.Qoodle.class).filter("qoodleId ==", 99).retrievedFields(true, "qeList").get();
 
             try {
@@ -228,19 +211,13 @@ public class Main {
             } catch (Exception e) {
                 return "[]";
             }
-        }
-        else
-        {
+        } else {
             return "ACCESSO VIETATO";
         }
     }
 
 
-
-
-
-
-    private static Response deleteQoodle(Response res,  Request req, Gson gson, Datastore datastore) {
+    private static Response deleteQoodle(Response res, Request req, Gson gson, Datastore datastore) {
 
 
         System.out.println("sono nel punto della delete");
@@ -248,20 +225,18 @@ public class Main {
 
         AuthObject checkIdentity = new AuthObject(req.headers("id_client"), req.headers("id_token"));
 
-        UserInfo suspectedUser =  gson.fromJson(checkIdentity.verify(gson), UserInfo.class);
+        UserInfo suspectedUser = gson.fromJson(checkIdentity.verify(gson), UserInfo.class);
 
         System.out.println(req.headers("owner") + "  " + suspectedUser.getEmail() + " uguaglianza : " + (req.headers("owner").equals(suspectedUser.getEmail())));
-        if(isLoggedIn(req) && (req.headers("owner").equals(suspectedUser.getEmail())) ) {
+        if (isLoggedIn(req) && (req.headers("owner").equals(suspectedUser.getEmail()))) {
 
             Qoodle.delete(targetId, datastore);
 
 
             res.status(200);
 
-                 }
-        else
-        {
-           res.status(401);
+        } else {
+            res.status(401);
         }
 
         return res;
@@ -270,9 +245,9 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        final String from= "http://qoodle.mondora.com";
-        final String how= "get";
-        final String head= "*";
+        final String from = "http://qoodle.mondora.com";
+        final String how = "get";
+        final String head = "*";
 
 
         org.mondora.qoodle.Inizialization init = new org.mondora.qoodle.Inizialization(from, how, head);
@@ -283,38 +258,32 @@ public class Main {
         datastore.ensureIndexes();
 
 
-        try{
+        try {
 
             Gson gson = new Gson();
 
 
-
-
             //LIST -aut
-            get("/qoodles", (req, res) ->getList(datastore, gson, req));
-
-
+            get("/qoodles", (req, res) -> getList(datastore, gson, req));
 
 
             //DETAILS -aut
-            get("/details/:id", (req, res) -> getDetails(datastore, gson, req) );
+            get("/details/:id", (req, res) -> getDetails(datastore, gson, req));
 
 
             //AUTHENTICATION
             post("/token", (req, res) -> showUserToken(gson, req));
 
 
-
-
             //VIEW -aut
 
-            get("/qoodle/:id", (req, res) ->   getQoodleView(gson, datastore, req) );
+            get("/qoodle/:id", (req, res) -> getQoodleView(gson, datastore, req));
             //-aut
-            post("/qoodle/:id", (req, res) ->       submitVotes(datastore, gson, req, res) );
+            post("/qoodle/:id", (req, res) -> submitVotes(datastore, gson, req, res));
 
 
             //CREATE -aut
-            post("/qoodles", (req, res) ->   saveQoodle("qoodleId", req, res, gson, datastore));
+            post("/qoodles", (req, res) -> saveQoodle("qoodleId", req, res, gson, datastore));
 
             //DELETE
             delete("/qoodle/:id", (req, res) -> deleteQoodle(res, req, gson, datastore));
@@ -322,8 +291,7 @@ public class Main {
             //-aut
             get("/create", (req, res) -> getQoodleElements(gson, datastore, req));
 
-
-        } catch(Exception ex) {
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
 
